47945883bb1681cd1efef25989e43ce32a4566e5
Hashem <hashemp206@yahoo.com>
2022-05-23 18:43:43 +0400

    Add Naurt Android SDK dependency

--- naurt_android/android/src/main/kotlin/com/naurt/naurt_android/NaurtAndroidPlugin.kt
+++ naurt_android/android/src/main/kotlin/com/naurt/naurt_android/NaurtAndroidPlugin.kt
@@ -8,6 +8,8 @@ import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
+import  com.naurt_kotlin_sdk.Naurt.INSTANCE as Naurt
+
 /** NaurtAndroidPlugin */
 class NaurtAndroidPlugin: FlutterPlugin, MethodCallHandler {
   /// The MethodChannel that will the communication between Flutter and native Android
